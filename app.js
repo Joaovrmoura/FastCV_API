c391678659c79858cdbc70398b46410d5209f114
joaovrmoura <joaovrmoura4@gmail.com>
2025-06-26 22:01:25 -0300

    proxy

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ import corsOptions from './src/config/cors.config.js';
 import rateLimiter from './src/config/rateLimit.config.js';
 
 const app = express();
-
+app.set('trust proxy', 1);
 app.use(helmet());
 app.use(cors(corsOptions));
 app.use(rateLimiter);
